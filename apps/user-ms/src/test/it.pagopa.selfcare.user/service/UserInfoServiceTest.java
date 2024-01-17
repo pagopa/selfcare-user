ec3647e168744850f1977f488820a32ab4386c1d
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2024-01-17 16:43:23 +0100

    Update unit tests

--- apps/user-ms/src/test/it.pagopa.selfcare.user/service/UserInfoServiceTest.java
+++ apps/user-ms/src/test/it.pagopa.selfcare.user/service/UserInfoServiceTest.java
@@ -1,11 +1,10 @@
 package service;
 
+import io.quarkus.mongodb.panache.reactive.ReactivePanacheQuery;
 import io.quarkus.panache.mock.PanacheMock;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.mongodb.MongoTestResource;
-import io.quarkus.test.vertx.RunOnVertxContext;
-import io.quarkus.test.vertx.UniAsserter;
 import io.smallrye.mutiny.Uni;
 import it.pagopa.selfcare.user.controller.response.UserInfoResponse;
 import it.pagopa.selfcare.user.entity.UserInfo;
@@ -14,8 +13,9 @@ import jakarta.inject.Inject;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @QuarkusTest
@@ -26,37 +26,34 @@ public class UserInfoServiceTest {
     private UserInfoServiceDefault userInfoService;
 
     @Test
-    @RunOnVertxContext
-    void findById(UniAsserter asserter) {
+    void findById() {
         final String id = new ObjectId().toHexString();
-        asserter.execute(() -> PanacheMock.mock(UserInfo.class));
-        asserter.execute(() -> when(UserInfo.findById(anyString()))
-                .thenAnswer(arg -> {
-                    UserInfo userInfo = (UserInfo) arg.getArguments()[0];
-                    userInfo.setId(ObjectId.get());
-                    return Uni.createFrom().nullItem();
-                }));
+        UserInfo userInfo = createDummyUserInfo();
+        PanacheMock.mock(UserInfo.class);
+        when(UserInfo.findById(any()))
+                .thenReturn(Uni.createFrom().item(userInfo));
         Uni<UserInfoResponse> response = userInfoService.findById(id);
         Assertions.assertNotNull(response);
     }
 
     @Test
-    @RunOnVertxContext
-    void findByUserId(UniAsserter asserter) {
+    void findByUserId() {
         final String userId = "userId";
-        mockfindUserInfo(asserter);
+        UserInfo userInfo = createDummyUserInfo();
+        PanacheMock.mock(UserInfo.class);
+        ReactivePanacheQuery query = Mockito.mock(ReactivePanacheQuery.class);
+        when(query.firstResult()).thenReturn(Uni.createFrom().item(userInfo));
+        when(UserInfo.find(any(), (Object) any()))
+                .thenReturn(query);
         Uni<UserInfoResponse> response = userInfoService.findByUserId(userId);
         Assertions.assertNotNull(response);
     }
 
-    void mockfindUserInfo(UniAsserter asserter) {
-        asserter.execute(() -> PanacheMock.mock(UserInfo.class));
-        asserter.execute(() -> when(UserInfo.find(anyString(), anyString()))
-                .thenAnswer(arg -> {
-                    UserInfo userInfo = (UserInfo) arg.getArguments()[0];
-                    userInfo.setId(ObjectId.get());
-                    return Uni.createFrom().nullItem();
-                }));
+    private UserInfo createDummyUserInfo() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setId(ObjectId.get());
+        userInfo.setUserId("userId");
+        return userInfo;
     }
 
 }
--- apps/user-ms/src/test/it.pagopa.selfcare.user/service/UserInstitutionServiceTest.java
+++ apps/user-ms/src/test/it.pagopa.selfcare.user/service/UserInstitutionServiceTest.java
@@ -1,10 +1,10 @@
 package service;
 
+import io.quarkus.mongodb.panache.reactive.ReactivePanacheQuery;
 import io.quarkus.panache.mock.PanacheMock;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.mongodb.MongoTestResource;
-import io.quarkus.test.vertx.RunOnVertxContext;
 import io.quarkus.test.vertx.UniAsserter;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
@@ -15,8 +15,9 @@ import jakarta.inject.Inject;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @QuarkusTest
@@ -27,46 +28,49 @@ public class UserInstitutionServiceTest {
     private UserInstitutionServiceDefault userInstitutionService;
 
     @Test
-    @RunOnVertxContext
-    void findById(UniAsserter asserter) {
+    void findById() {
         final String id = new ObjectId().toHexString();
-        asserter.execute(() -> PanacheMock.mock(UserInstitution.class));
-        asserter.execute(() -> when(UserInstitution.findById(anyString()))
-                .thenAnswer(arg -> {
-                    UserInstitution userInstitution = (UserInstitution) arg.getArguments()[0];
-                    userInstitution.setId(ObjectId.get());
-                    return Uni.createFrom().nullItem();
-                }));
+        UserInstitution userInstitution = createDummyUserInstitution();
+        PanacheMock.mock(UserInstitution.class);
+        when(UserInstitution.findById(any()))
+                .thenReturn(Uni.createFrom().item(userInstitution));
         Uni<UserInstitutionResponse> response = userInstitutionService.findById(id);
         Assertions.assertNotNull(response);
     }
 
     @Test
-    @RunOnVertxContext
-    void findByInstitutionId(UniAsserter asserter) {
+    void findByInstitutionId() {
         final String institutionId = "institutionId";
-        mockfindUserInstitution(asserter);
+        UserInstitution userInstitution = createDummyUserInstitution();
+        PanacheMock.mock(UserInstitution.class);
+        ReactivePanacheQuery query = Mockito.mock(ReactivePanacheQuery.class);
+        when(query.firstResult()).thenReturn(Uni.createFrom().item(userInstitution));
+        when(UserInstitution.find(any(), (Object) any()))
+                .thenReturn(query);
         Uni<UserInstitutionResponse> response = userInstitutionService.findByInstitutionId(institutionId);
         Assertions.assertNotNull(response);
     }
 
     @Test
-    @RunOnVertxContext
     void findByUserId(UniAsserter asserter) {
         final String userId = "userId";
-        mockfindUserInstitution(asserter);
+        UserInstitution userInstitution = createDummyUserInstitution();
+        PanacheMock.mock(UserInstitution.class);
+        ReactivePanacheQuery query = Mockito.mock(ReactivePanacheQuery.class);
+        when(query.firstResult()).thenReturn(Uni.createFrom().item(userInstitution));
+        when(UserInstitution.find(any(), (Object) any()))
+                .thenReturn(query);
         Multi<UserInstitutionResponse> response = userInstitutionService.findByUserId(userId);
         Assertions.assertNotNull(response);
     }
 
-    void mockfindUserInstitution(UniAsserter asserter) {
-        asserter.execute(() -> PanacheMock.mock(UserInstitution.class));
-        asserter.execute(() -> when(UserInstitution.find(anyString(), anyString()))
-                .thenAnswer(arg -> {
-                    UserInstitution userInstitution = (UserInstitution) arg.getArguments()[0];
-                    userInstitution.setId(ObjectId.get());
-                    return Uni.createFrom().nullItem();
-                }));
+    private UserInstitution createDummyUserInstitution() {
+        UserInstitution userInstitution = new UserInstitution();
+        userInstitution.setId(ObjectId.get());
+        userInstitution.setUserId("userId");
+        userInstitution.setInstitutionId("institutionId");
+        return userInstitution;
     }
+
 
 }
