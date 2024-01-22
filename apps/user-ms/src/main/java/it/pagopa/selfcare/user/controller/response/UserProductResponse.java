e68104678ee59e744db3d191bb277c8e22850ea9
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2024-01-22 18:09:41 +0100

    Added pdv rest client and respose resource object

--- apps/user-ms/src/main/java/it/pagopa/selfcare/user/controller/response/UserProductResponse.java
+++ apps/user-ms/src/main/java/it/pagopa/selfcare/user/controller/response/UserProductResponse.java
@@ -1,4 +1,9 @@
 package it.pagopa.selfcare.user.controller.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class UserProductResponse {
 }
--- apps/user-ms/src/main/java/it/pagopa/selfcare/user/service/UserServiceImpl.java
+++ apps/user-ms/src/main/java/it/pagopa/selfcare/user/service/UserServiceImpl.java
@@ -4,12 +4,20 @@ import io.smallrye.mutiny.Multi;
 import it.pagopa.selfcare.user.controller.response.UserProductResponse;
 import it.pagopa.selfcare.user.entity.UserInstitution;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import lombok.RequiredArgsConstructor;
-
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.openapi.quarkus.user_registry_json.api.UserApi;
 
 @RequiredArgsConstructor
 @ApplicationScoped
 public class UserServiceImpl implements UserService {
+
+    @RestClient
+    @Inject
+    private UserApi userRegistryApi;
+
+    private static final String USERS_WORKS_FIELD_LIST = "fiscalCode,familyName,name,workContacts";
 
     public Multi<UserProductResponse> getUserProductsByInstitution(String institutionId) {
         Multi<UserInstitution> userInstitutions =  UserInstitution.find("institutionId", institutionId).stream();
