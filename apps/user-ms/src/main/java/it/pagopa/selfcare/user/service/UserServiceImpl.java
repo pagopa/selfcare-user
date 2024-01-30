5784f92c5fb921ed14a0560aea4cea80026ddddf
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-01-30 17:13:32 +0100

    merge main

--- apps/user-ms/src/main/java/it/pagopa/selfcare/user/service/UserServiceImpl.java
+++ apps/user-ms/src/main/java/it/pagopa/selfcare/user/service/UserServiceImpl.java
@@ -24,6 +24,7 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.openapi.quarkus.user_registry_json.api.UserApi;
 import org.openapi.quarkus.user_registry_json.model.UserResource;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
