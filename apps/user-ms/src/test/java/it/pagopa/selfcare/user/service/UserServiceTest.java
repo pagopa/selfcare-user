9404bdae6a7c1a1d96562c07d298841f57849ce9
empassaro <emanuele.passaro@emeal.nttdata.com>
2024-02-01 09:28:08 +0100

    fix: removed unused import

--- apps/user-ms/src/test/java/it/pagopa/selfcare/user/service/UserServiceTest.java
+++ apps/user-ms/src/test/java/it/pagopa/selfcare/user/service/UserServiceTest.java
@@ -52,7 +52,6 @@ import static it.pagopa.selfcare.user.constant.CustomError.USER_TO_UPDATE_NOT_FO
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
