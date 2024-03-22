ba6b2ba85548f7648d0c5f2609a194513e0b9165
manuraf <manuel.rafeli@pagopa.it>
2024-03-22 17:24:29 +0100

    move mongodb infra to user-ms

--- infra/container_apps/user-ms/variables.tf
+++ infra/container_apps/user-ms/variables.tf
@@ -66,3 +66,9 @@ variable "secrets_names" {
   type        = map(string)
   description = "KeyVault secrets to get values from"
 }
+
+variable "cosmosdb_mongodb_throughput" {
+  type        = number
+  description = "The throughput of the MongoDB database (RU/s). Must be set in increments of 100. The minimum value is 400. This must be set upon database creation otherwise it cannot be updated without a manual terraform destroy-apply."
+  default     = 1000
+}
