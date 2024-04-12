a5f29539cd50c25941702ace0c30fb92c8bffc5a
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-04-12 14:40:42 +0200

    fix UserInfo model

--- apps/user-cdc/src/main/java/it/pagopa/selfcare/user/event/entity/UserInfo.java
+++ apps/user-cdc/src/main/java/it/pagopa/selfcare/user/event/entity/UserInfo.java
@@ -1,24 +1,26 @@
 package it.pagopa.selfcare.user.event.entity;
 
 import io.quarkus.mongodb.panache.common.MongoEntity;
-import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntityBase;
+import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.FieldNameConstants;
 import org.bson.codecs.pojo.annotations.BsonId;
 import org.bson.codecs.pojo.annotations.BsonProperty;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @MongoEntity(collection = "userInfo")
 @FieldNameConstants(asEnum = true)
-public class UserInfo extends ReactivePanacheMongoEntityBase {
+public class UserInfo extends ReactivePanacheMongoEntity {
 
     @BsonId
     @BsonProperty("_id")
     private String userId;
-    private List<UserInstitutionRole> institutions;
+
+    private List<UserInstitutionRole> institutions = new ArrayList<>();
 
 }
