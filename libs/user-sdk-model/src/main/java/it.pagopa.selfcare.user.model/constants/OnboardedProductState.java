23e378a4c47d2da681b567ae74350e488937935b
manuraf <manuel.rafeli@pagopa.it>
2024-06-18 14:15:35 +0200

    sdk-model module

---
+++ libs/user-sdk-model/src/main/java/it.pagopa.selfcare.user.model/constants/OnboardedProductState.java
@@ -0,0 +1,10 @@
+package it.pagopa.selfcare.user.model.constants;
+
+public enum OnboardedProductState {
+    ACTIVE,
+    PENDING,
+    TOBEVALIDATED,
+    SUSPENDED,
+    DELETED,
+    REJECTED
+}
