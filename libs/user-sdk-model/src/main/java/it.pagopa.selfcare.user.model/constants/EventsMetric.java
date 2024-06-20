605e248f75efa619e4dac9509b1b81f9be9715fc
manuraf <manuel.rafeli@pagopa.it>
2024-06-20 15:30:41 +0200

    metrics name on sdk-model

---
+++ libs/user-sdk-model/src/main/java/it.pagopa.selfcare.user.model/constants/EventsMetric.java
@@ -0,0 +1,10 @@
+package it.pagopa.selfcare.user.model.constants;
+
+public class EventsMetric {
+
+
+    public static final String EVENTS_USER_INSTITUTION_FAILURE = "EventsUserInstitution_failures";
+    public static final String EVENTS_USER_INSTITUTION_SUCCESS = "EventsUserInstitution_success";
+    public static final String EVENTS_USER_INSTITUTION_PRODUCT_FAILURE = "EventsUserInstitutionProduct_failures";
+    public static final String EVENTS_USER_INSTITUTION_PRODUCT_SUCCESS = "EventsUserInstitutionProduct_success";
+}
---
+++ libs/user-sdk-model/src/main/java/it.pagopa.selfcare.user.model/constants/EventsName.java
@@ -0,0 +1,7 @@
+package it.pagopa.selfcare.user.model.constants;
+
+public class EventsName {
+
+    public static final String EVENT_USER_MS_NAME = "USER-MS";
+    public static final String EVENT_USER_CDC_NAME = "USER-MS";
+}
