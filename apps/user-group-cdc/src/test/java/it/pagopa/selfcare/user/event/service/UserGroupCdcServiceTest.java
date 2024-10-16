7f74f97029f91757d6870d7821b8dbf4d223b1f0
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-10-16 14:33:45 +0200

    [SELC-5740] add assertion on test

--- apps/user-group-cdc/src/test/java/it/pagopa/selfcare/user/event/service/UserGroupCdcServiceTest.java
+++ apps/user-group-cdc/src/test/java/it/pagopa/selfcare/user/event/service/UserGroupCdcServiceTest.java
@@ -9,12 +9,17 @@ import it.pagopa.selfcare.user.client.EventHubRestClient;
 import it.pagopa.selfcare.user.event.UserGroupCdcService;
 import it.pagopa.selfcare.user.event.entity.UserGroupEntity;
 import it.pagopa.selfcare.user.event.mapper.UserGroupNotificationMapper;
+import it.pagopa.selfcare.user.model.UserGroupNotificationToSend;
 import jakarta.inject.Inject;
 import org.bson.BsonDocument;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.mockito.Spy;
+
+import java.util.Set;
 
 import static org.mockito.Mockito.*;
 
@@ -39,14 +44,23 @@ public class UserGroupCdcServiceTest {
         when(document.getFullDocument()).thenReturn(userGroupEntity);
         BsonDocument bsonDocument = Mockito.mock(BsonDocument.class);
         when(document.getDocumentKey()).thenReturn(bsonDocument);
+
+        ArgumentCaptor<UserGroupNotificationToSend> notification = ArgumentCaptor.forClass(UserGroupNotificationToSend.class);
         userGroupCdcService.consumerToSendScUserGroupEvent(document);
         verify(eventHubRestClient, times(1)).
-                sendUserGroupMessage(any());
+                sendUserGroupMessage(notification.capture());
+        Assertions.assertEquals(userGroupEntity.getId(), notification.getValue().getId());
+        Assertions.assertEquals(userGroupEntity.getInstitutionId(), notification.getValue().getInstitutionId());
+        Assertions.assertEquals(userGroupEntity.getProductId(), notification.getValue().getProductId());
+        Assertions.assertEquals(2, notification.getValue().getMembers().size());
     }
 
     UserGroupEntity dummyUserGroup() {
         UserGroupEntity userGroupEntity = new UserGroupEntity();
         userGroupEntity.setId("UserGroupId");
+        userGroupEntity.setInstitutionId("InstitutionId");
+        userGroupEntity.setProductId("ProductId");
+        userGroupEntity.setMembers(Set.of("Member1", "Member2"));
         return userGroupEntity;
     }
 }
