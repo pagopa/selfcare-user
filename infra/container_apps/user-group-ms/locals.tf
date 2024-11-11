6445a8fcf49cfd588f278a1355e6a6b68ae2b327
empassaro <emanuele.passaro@emeal.nttdata.com>
2024-11-11 13:20:33 +0100

    [SELC-5957] feat: moved collection definition under container_apps/user-group-ms

--- infra/container_apps/user-group-ms/locals.tf
+++ infra/container_apps/user-group-ms/locals.tf
@@ -1,7 +1,13 @@
 locals {
   pnpg_suffix = var.is_pnpg == true ? "-pnpg" : ""
+  mongo_pnpg_suffix =var.is_pnpg == true ? "-weu-pnpg" : ""
   project     = "selc-${var.env_short}"
 
   container_app_environment_name = "${local.project}${local.pnpg_suffix}-${var.cae_name}"
   ca_resource_group_name         = "${local.project}-container-app${var.suffix_increment}-rg"
+
+  mongo_db = {
+    mongodb_rg_name               = "selc-${var.env_short}${local.mongo_pnpg_suffix}-cosmosdb-mongodb-rg",
+    cosmosdb_account_mongodb_name = "selc-${var.env_short}${local.mongo_pnpg_suffix}-cosmosdb-mongodb-account"
+  }
 }
