e52fbc4d21e016025dbb36ad308b38d39c763d9c
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2026-02-10 13:09:38 +0100

    [SELC-8117] With Spring Boot 3, @EnableMongoAuditing must be closer to the @SpringBootApplication annotated class

--- apps/user-group-ms/src/main/java/it/pagopa/selfcare/user_group/config/CoreConfig.java
+++ apps/user-group-ms/src/main/java/it/pagopa/selfcare/user_group/config/CoreConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
-@EnableMongoAuditing(modifyOnCreate = false)
 @PropertySource("classpath:config/core-config.properties")
 public class CoreConfig {
     @Bean
