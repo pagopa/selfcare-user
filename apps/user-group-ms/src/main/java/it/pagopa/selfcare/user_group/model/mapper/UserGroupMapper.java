f96cfe2c921f60b17f17dbe33755486da1ede647
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2026-02-13 12:36:01 +0100

    [SELC-8117] Added a new mapping function to convert a comma separated list of strings to a spring Sort object

--- apps/user-group-ms/src/main/java/it/pagopa/selfcare/user_group/model/mapper/UserGroupMapper.java
+++ apps/user-group-ms/src/main/java/it/pagopa/selfcare/user_group/model/mapper/UserGroupMapper.java
@@ -5,11 +5,14 @@ import it.pagopa.selfcare.user_group.model.*;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
+import org.springframework.data.domain.Sort;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Mapper(componentModel = "spring")
 public interface UserGroupMapper {
@@ -39,6 +42,33 @@ public interface UserGroupMapper {
                 .stream()
                 .map(UUID::toString)
                 .collect(Collectors.toSet());
+    }
+
+    default Sort toSort(String sort) {
+        if (sort == null || sort.isBlank()) {
+            return Sort.unsorted();
+        }
+
+        final List<String> sortParams = Stream.of(sort.split(",")).map(String::trim).toList();
+        final List<Sort.Order> orders = new ArrayList<>();
+        String prop = null;
+        for (String s : sortParams) {
+            if (prop == null) {
+                prop = s;
+            } else if (s.equalsIgnoreCase(Sort.Direction.ASC.name()) || s.equalsIgnoreCase(Sort.Direction.DESC.name())) {
+                orders.add(new Sort.Order(Sort.Direction.fromString(s), prop));
+                prop = null;
+            } else {
+                orders.add(new Sort.Order(Sort.Direction.ASC, prop));
+                prop = s;
+            }
+        }
+
+        if (prop != null) {
+            orders.add(new Sort.Order(Sort.Direction.ASC, prop));
+        }
+
+        return Sort.by(orders);
     }
 
 }
